052e5aca582b2300efa54d8fd0966f1f69d59c4a
omar <thejarvisgroupllc@gmail.com>
2021-03-06 07:08:15 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function subtract(a, b) {
     return a - b;
 }
 
-function multiply( a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide( a, b) {
+function divide(a, b) {
     return a / b;
 }
 
@@ -22,3 +22,11 @@ function increment(a) {
 function decrement(a) {
     return a -= 1;
 }
+
+function makeInt(string) {
+    return parseInt(string, 10)
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string)
+  }
